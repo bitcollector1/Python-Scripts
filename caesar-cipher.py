3f1769917dfbd9b2c0e03d1a7e6d6989b0a2e1a0
Me <me@macbook-2-wifi.justmynet.lan>
2021-01-04 23:39:32 -0800

    converted from C++ to Python

---
+++ caesar-cipher.py
@@ -0,0 +1,22 @@
+# Cipher should eliminate capitalization, spaces & punctuation for maximum effectivness 
+import string
+
+def caesar():
+    message = input("\nPlease input your message.\n").replace(" ","").lower().strip(string.punctuation)
+    shift = int(input("What is the shift?\n "))
+    
+    if (shift % 26 == 0):
+        return("There can be no shift if divisible by 26")
+    
+    # decode option is simple math - inverse operations
+    if "e" not in input("\nType 'e' or 'E' to encode, anything else will decode\n"):
+        shift = len(string.ascii_letters) - shift
+    
+    alphabet = string.ascii_letters.lower()
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+    table = str.maketrans(alphabet, shifted_alphabet)
+    return message.translate(table)
+
+
+print(caesar())
+
