720345ebfb4209589bac577e325c2a2d1a02db48
bitcollector1 <duane.noland@gmail.com>
2021-01-14 15:40:29 -0800

    message

--- easy.py
+++
@@ -1,7 +0,0 @@
-# Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple
-
-def tup(*args):
-    print(list(args))
-    print(args)
-
-tup(input("\nPlease enter a sequence of numbers\n"))
