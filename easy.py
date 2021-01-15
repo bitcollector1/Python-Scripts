0bad24bee6ed0baecc18dc8d7e0efeec7aa634b4
bitcollector1 <duane.noland@gmail.com>
2021-01-14 20:10:41 -0800

    Python3 Practice Problems

---
+++ easy.py
@@ -0,0 +1,30 @@
+# Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple
+
+def tup(*args):
+    print(list(args))
+    print(args)
+
+tup(input("\nPlease enter a sequence of numbers\n"))
+
+
+# Test for palindrome - Rats live on no evil star
+
+def palindrome(message):
+    while message:
+        if message[0] == message[-1]:
+            message.pop(0)
+            message.pop()
+        else:
+            break
+
+    if not message:
+        print("Your string IS a palindrome")
+    else:
+        print("Your string IS NOT a palindrome")
+
+
+if __name__ == '__main__':
+
+    palindrome(list(input("\nInput string to test palindrome\n").replace(" ", "").lower()))
+
+
