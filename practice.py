59c916624b198fe58af51e352bae70e129ae497d
bitcollector1 <50723251+bitcollector1@users.noreply.github.com>
2021-12-01 22:18:16 -0800

    maxSubArray one liner with lambda

--- practice.py
+++ practice.py
@@ -113,3 +113,16 @@ class Solution:
 my_solution = Solution()
 my_solution.maxSubArray(nums)
 
+############################################################################################################################################
+# Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+############################################################################################################################################
+
+from itertools import accumulate
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        return max(accumulate(nums, lambda a, b: max(a, 0) + b))
+    
+    
+my_solution = Solution()
+my_solution.maxSubArray(nums)
