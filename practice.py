2812d772e650d875ab6f76174b309660a37ed970
bitcollector1 <50723251+bitcollector1@users.noreply.github.com>
2021-12-01 21:25:12 -0800

    Dynamic Programming, Kadane's Algorithm

--- practice.py
+++ practice.py
@@ -88,3 +88,28 @@ res = sum(1 for i in range(len(ini_str)) if ini_str.startswith(sub_str, i))
 
 print(f"Number of substrings {sub_str} found: {res} ")
 
+############################################################################################################################################
+# Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+############################################################################################################################################
+
+# Dynamic Programming, Kadane's Algorithm
+from typing import List
+
+nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # Initialize our variables using the first element.
+        current_subarray = max_subarray = nums[0]
+        
+        # Start with the 2nd element since we already used the first one.
+        for num in nums[1:]:
+            # If current_subarray is negative, throw it away. Otherwise, keep adding to it.
+            current_subarray = max(num, current_subarray + num)
+            max_subarray = max(max_subarray, current_subarray)
+            
+        return max_subarray
+    
+my_solution = Solution()
+my_solution.maxSubArray(nums)
+
